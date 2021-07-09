ba77ca031c9fc4547f2be4bd461adba97953af73
Manuel Rodriguez <lolo.edinburgh@gmail.com>
2021-07-09 20:53:50 +0100

    added json-server and StreamCreate.js plus all action creators

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,4 +1,6 @@
-import { SIGN_IN,SIGN_OUT} from "./types";
+import {CREATE_STREAM, DELETE_STREAM, EDIT_STREAM, FETCH_STREAM, FETCH_STREAMS, SIGN_IN, SIGN_OUT} from "./types";
+import streams from "../api/streams";
+
 
 export const signIn = (userId) => {
     return {
@@ -7,9 +9,36 @@ export const signIn = (userId) => {
     }
 }
 
-export const signOut = () =>{
+export const signOut = () => {
     return {
         type: SIGN_OUT
     }
 }
+
+export const createStream = formValues => async dispatch => {
+    const response = await streams.post('/streams', formValues)
+    dispatch({type: CREATE_STREAM, payload: response.data})
+}
+
+export const fetchStreams = () => async dispatch => {
+    const response = await streams.get('/streams')
+    dispatch({type: FETCH_STREAMS, payload: response.data})
+}
+
+export const fetchStream = (id) => async dispatch => {
+    const response = await streams.get(`/streams/${id}`)
+    dispatch({type: FETCH_STREAM, payload: response.data})
+}
+
+export const editStream = (id, formValues) => async dispatch => {
+    const response = await streams.put(`/streams/${id}`, formValues)
+    dispatch({type: EDIT_STREAM, payload: response.data})
+}
+
+export const deleteStream = id => async dispatch => {
+    await streams.delete(`/streams/${id}`)
+    dispatch({type: DELETE_STREAM, payload: id})
+}
+
+
 
--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -1,9 +1,68 @@
 import React from 'react'
+import { Field, reduxForm } from "redux-form";
+import { connect} from "react-redux";
+import { createStream } from "../../actions";
 
-const StreamCreate = () => {
-    return (
-        <div>Create</div>
-    )
+class StreamCreate extends React.Component {
+
+    renderError = ({touched, error}) => {
+        if(touched && error){
+            return (
+            <div className={`ui error message`}>
+                <div className="header">
+                    {error}
+                </div>
+            </div>
+        )
+        }
+
+    }
+
+
+    renderInput = ({input, label, meta}) =>  {
+        const className = `field ${meta.error && meta.touched ? 'error' : ""}`
+        return(
+            <div className={className}>
+                <label>
+                    {label}
+                </label>
+                <input{...input}/>
+                {this.renderError(meta)}
+            </div>
+        )
+    }
+
+
+    onSubmit = (fromValues) => {
+        this.props.createStream(fromValues)
+    }
+
+    render() {
+        return(
+            <form onSubmit={this.props.handleSubmit(this.onSubmit)} className={`ui form error`}>
+                <Field name={`title`} label={"Enter title"} component={this.renderInput} />
+                <Field name={`description`} label={"Enter description"} component={this.renderInput}/>
+                <button className={`ui button primary`}>Submit</button>
+            </form>
+        )
+    }
 }
 
-export default StreamCreate;
+const validate = (formValues) => {
+    const errors = {}
+    if(!formValues.title){
+        errors.title = "you must enter a title"
+    }
+    if(!formValues.description){
+        errors.description = 'you must enter a description'
+    }
+    return errors
+
+}
+
+const formWrapped = reduxForm({
+    form: 'streamCreate',
+    validate
+}) (StreamCreate);
+
+export default connect(null, {createStream})(formWrapped)
